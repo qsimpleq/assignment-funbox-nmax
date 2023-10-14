c8f8b9a6d7f703e247ab58d0b06c7a836c2f6c33
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-10-14 14:49:14 +0500

    Add tests

--- spec/nmax_spec.rb
+++ spec/nmax_spec.rb
@@ -1,12 +1,37 @@
 # frozen_string_literal: true
 
+def expect_pair_equal(pair)
+  expect(pair[0]).to eq(pair[1])
+end
+
+def pair(name, options = {})
+  input, output = load_fixture_pair(name)
+  stream = StringIO.new(input.to_s)
+  klass = described_class.build({ number_max_length: 3, numbers_limit: 10, stream: stream }.merge(options))
+
+  [klass.sorted_result.join("\n"), output.strip]
+end
+
+def load_fixture_pair(name)
+  [load_fixture(name), load_fixture("#{name}_result")]
+end
+
 RSpec.describe Nmax do
   it 'has a version number' do
-    pp '###'
     expect(Nmax::VERSION).not_to be nil
   end
 
-  # it 'does something useful' do
-  #   expect(false).to eq(true)
-  # end
+  it 'check ByChunk' do
+    options = { parser: :ByChunk }
+    expect_pair_equal pair(:no_numbers, options)
+    expect_pair_equal pair(:one_numbers, options)
+    expect_pair_equal pair(:ten_numbers, options)
+  end
+
+  it 'check ByLine' do
+    options = { parser: :ByLine }
+    expect_pair_equal pair(:no_numbers, options)
+    expect_pair_equal pair(:one_numbers, options)
+    expect_pair_equal pair(:ten_numbers, options)
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,15 @@
 require 'rspec'
 require 'nmax'
 
+module TestHelpers
+  def load_fixture(file_name)
+    File.read("#{File.dirname(__FILE__)}/fixtures/#{file_name}.txt")
+  end
+end
+
 RSpec.configure do |config|
+  config.include TestHelpers
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
 
